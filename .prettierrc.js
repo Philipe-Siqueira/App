8a76b3d19d6d1d30f070268fec87b137b135b8a2
= <philipe.tec@gmail.com>
2021-10-10 14:55:01 -0300

    :art: Editor config + EsLint + Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: false, 
+  trailingComma: 'none',
+	arrowParens: 'avoid',
+  tabWidth: 2,
+  semi: true,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: always
+};
